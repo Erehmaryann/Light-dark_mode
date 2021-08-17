971d65a09ff9a876fa86a1811e186d0b8f1a70c7
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-17 16:59:19 -0700

    made it a bit responsive

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switches theme dynamically
-function switchTheme() {}
+function switchTheme(e) {
+  console.log(e.target.checked);
+}
 
 // Event listeners
 toggleSwitch.addEventListener("change", switchTheme);
