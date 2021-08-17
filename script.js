b444a02f0acf264ee2e85d192f731a09e714e19b
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-17 16:34:52 -0700

    added an event listener to the toggle switch element

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // Variables
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switches theme dynamically
+function switchTheme() {}
+
+// Event listeners
+toggleSwitch.addEventListener("change", switchTheme);
