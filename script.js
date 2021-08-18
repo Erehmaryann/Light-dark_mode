e6ea48f15346e83cf5ddd6d0d9a0f88ac1d30852
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-18 16:04:10 -0700

    refactored the toggle icon second child logic in light mode

--- script.js
+++ script.js
@@ -23,8 +23,7 @@ function lightMode() {
   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
   toggleIcon.children[0].textContent = "Light Mode";
-  toggleIcon.children[1].classList.remove("fa-moon");
-  toggleIcon.children[1].classList.add("fa-sun");
+  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
   image1.setAttribute("src", "./img/undraw_proud_coder_light.svg");
   image2.setAttribute("src", "./img/undraw_feeling_proud_light.svg");
   image3.setAttribute("src", "./img/undraw_conceptual_idea_light.svg");
