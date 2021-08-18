07333f00600754562741293da66aa78b2e252f2c
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-18 15:02:33 -0700

    created a dark mode function

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ const image2 = document.querySelector("#image2");
 const image3 = document.querySelector("#image3");
 const textBox = document.querySelector("#textbox");
 
+//dark mode styles
+function darkMode() {}
+
 // Switches theme dynamically
 function switchTheme(e) {
   if (e.target.checked) {
     //returns the root Element of the document(html)
     document.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
   } else {
     document.documentElement.removeAttribute("data-theme");
+    lightMode();
   }
 }
 
