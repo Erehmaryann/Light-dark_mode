4687fe43619f221e0ebc76dd8398e1ec1e8dd165
Ereh Maryann <edwardmaryann09@gmail.com>
2021-08-18 16:12:40 -0700

    added image mode func to dark/light mode func

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const image3 = document.querySelector("#image3");
 const textBox = document.querySelector("#text-box");
 
 //Dark or Light theme Images
-function imageMode(color) {}
+function imageMode(color) {
+  image1.setAttribute("src", `./img/undraw_proud_coder_${color}.svg`);
+  image2.setAttribute("src", `./img/undraw_feeling_proud_${color}.svg`);
+  image3.setAttribute("src", `./img/undraw_conceptual_idea_${color}.svg`);
+}
 
 //Dark mode styles
 function darkMode() {
@@ -16,9 +20,7 @@ function darkMode() {
   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
   toggleIcon.children[0].textContent = "Dark Mode";
   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-  image1.setAttribute("src", "./img/undraw_proud_coder_dark.svg");
-  image2.setAttribute("src", "./img/undraw_feeling_proud_dark.svg");
-  image3.setAttribute("src", "./img/undraw_conceptual_idea_dark.svg");
+  imageMode("dark");
 }
 
 // Light mode styles
@@ -27,9 +29,7 @@ function lightMode() {
   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
   toggleIcon.children[0].textContent = "Light Mode";
   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  image1.setAttribute("src", "./img/undraw_proud_coder_light.svg");
-  image2.setAttribute("src", "./img/undraw_feeling_proud_light.svg");
-  image3.setAttribute("src", "./img/undraw_conceptual_idea_light.svg");
+  imageMode("light");
 }
 
 // Switches theme dynamically
